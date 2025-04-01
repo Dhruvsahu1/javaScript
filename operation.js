901c49b9170a8b3aa54ad3fe6b5efcf95434bd24
Dhruv <dhruvsahu7355@gmail.com>
2025-04-01 12:28:53 +0530

    perform some basic operations and used some tricky question to solve it

---
+++ operation.js
@@ -0,0 +1,68 @@
+let value = 3;
+let negVal = -value;
+// console.log(negVal); // Output: -3
+
+// console.log(2+2); // Output: 4
+// console.log(2-2); // Output: 0
+// console.log(2*2); // Output: 4
+// console.log(2**3); // Output: 8
+// console.log(2/2); // Output: 1
+// console.log(2%2); // Output: 0
+
+
+
+// let str1 = "Hello";
+// let str2 = " Jarwis";
+// let str3 = str1 + str2;
+// console.log(str3); // Output: Hello Jarwis
+
+// console.log(1+"2");// Output: "12"
+// console.log(1+"2"+3);// Output: "123"
+// console.log(1+2+"3");// Output: "33"
+// console.log("1"+2+3);// Output: "123"
+
+// for more tc39.js read type conversion part 
+
+
+// console.log(+true); // Output: 1
+// console.log(typeof +true); // Output: number
+
+// console.log(+false); // Output: 0
+
+// console.log(+null); // Output: 0
+// console.log(+undefined); // Output: NaN
+// console.log(true+); it is not allowed 
+
+// console.log(+""); // Output: 0
+
+// prefix and postfix 
+// let num = 1;
+// console.log(++num); // Output: 2 prefix increament
+// console.log(num); // Output: 2
+// console.log(num++); // Output: 2 postfix increament
+// console.log(num); // Output: 3
+
+
+// let n = 5;
+// console.log(--n); // Output: 4 prefix decreament
+// console.log(n); // Output: 4    
+// console.log(n--); // Output: 4 postfix decreament
+// console.log(n); // Output: 3
+ 
+// try mdn docs for more information
+
+
+// Important tricky question
+
+console.log(+"123"); // Output: 123
+console.log(+"abc"); // Output: NaN
+
+
+
+
+
+
+
+
+
+
