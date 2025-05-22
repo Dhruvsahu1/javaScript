0295770620a9fb364bb950d20597684d2ba9817b
Dhruv <dhruvsahu7355@gmail.com>
2025-05-22 10:28:54 +0530

    First color changer project is completed

---
+++ Projects/BackgroundColorChanger/script.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('button');
+const body = document.querySelector('body');
+console.log(buttons);
+buttons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        console.log(e.target);
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id
+        }
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id    
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id    
+        }
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id    
+        }
+        if(e.target.id === 'gray'){
+            body.style.backgroundColor = e.target.id    
+        }
+    })
+})
