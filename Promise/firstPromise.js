93599100820c8d6db3afb47ed64343034f6a05d8
Dhruv <dhruvsahu7355@gmail.com>
2025-05-30 22:11:46 +0530

    basic Defination of Promise in javascript

---
+++ Promise/firstPromise.js
@@ -0,0 +1,18 @@
+// Promise => A Promise is a proxy for a value not necessarily known when the promise is created. It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, the asynchronous method returns a promise to supply the value at some point in the future.
+
+// A Promise is in one of these states:
+
+// pending:- initial state, neither fulfilled nor rejected.
+// fulfilled:- meaning that the operation was completed successfully.
+// rejected:- meaning that the operation failed.
+
+
+// Promise Basic
+// fetch('https://something.com').then().catch().finally()
+// this is the basic structure for promises 
+// then will execute when the resolved state 
+// and catch will execute when the  reject state
+// finally is execute at the end  when neither [then] run or [catch] or may they both not execute
+
+
+// HOW TO CREATE A PROMISE
